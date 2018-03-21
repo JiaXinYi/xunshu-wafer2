f58428ed3c78de1af8bc1a9f99a23603c48e73c8
jiaxin <924463684@qq.com>
2018-03-22 00:44:35 +0800

    创建新表单

---
+++ server/tools/cBooklist.sql
@@ -0,0 +1,32 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50717
+ Source Host           : localhost
+ Source Database       : cAuth
+
+ Target Server Type    : MySQL
+ Target Server Version : 50717
+ File Encoding         : utf-8
+
+ Date: 08/10/2017 22:22:52 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `cSessionInfo`
+-- ----------------------------
+DROP TABLE IF EXISTS `cBooklist`;
+CREATE TABLE `cBooklist` (
+  `open_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `uuid` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `book_info` varchar(2048) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`uuid`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户保存书单列表';
+
+SET FOREIGN_KEY_CHECKS = 1;
